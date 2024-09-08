a1a5df4925cf5f864d634438ca12bec3f5766c60
shahsamved <shahsamved@gmail.com>
2024-09-08 13:33:29 -0400

    cors

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -142,6 +142,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',  
     'http://127.0.0.1:3000',
